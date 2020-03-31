31041c1330cb0562b1badaf8f6a854a796dfb513
zhangwh <744004249@qq.com>
2020-04-01 00:55:36 +0800

    第一次测试本地工程添加到git

---
+++ git-test-progect/src/com/bw/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.bw.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+
+    }
+
+}
